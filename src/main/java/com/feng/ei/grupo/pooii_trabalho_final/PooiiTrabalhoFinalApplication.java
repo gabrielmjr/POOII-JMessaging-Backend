270c384361644634ad5ee748b8c64f1d9fd37ea3
Gabriel MJr <me@gabrielmjr.engineer>
2025-10-29 19:23:31 +0200

    Initial commit

---
+++ src/main/java/com/feng/ei/grupo/pooii_trabalho_final/PooiiTrabalhoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.feng.ei.grupo.pooii_trabalho_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooiiTrabalhoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooiiTrabalhoFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feng/ei/grupo/pooii_trabalho_final/PooiiTrabalhoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feng.ei.grupo.pooii_trabalho_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooiiTrabalhoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
